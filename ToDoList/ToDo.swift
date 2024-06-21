3b5204e398379c77ad7b5b5879bf20cb227d46c5
Rafael Rodriguez <rafael.nc@me.com>
2024-06-21 14:27:30 -0400

    Ch. 5.6 Completion of the Model (Data Model) to ToDo List app

---
+++ ToDoList/ToDo.swift
@@ -0,0 +1,8 @@
+//
+//  ToDo.swift
+//  ToDoList
+//
+//  Created by Rafael Rodriguez on 6/20/24.
+//
+
+import Foundation
---
+++ ToDoList/ToDosViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ToDosViewModel.swift
+//  ToDoList
+//
+//  Created by Rafael Rodriguez on 6/20/24.
+//
+
+import Foundation

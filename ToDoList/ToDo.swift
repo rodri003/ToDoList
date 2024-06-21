9efd6963ef1689a4102e671d8e72a5c92719845c
Rafael Rodriguez <rafael.nc@me.com>
2024-06-21 14:28:41 -0400

    Ch. 5.6 Completion of Model (Data Model) to ToDo List app

--- ToDoList/ToDo.swift
+++ ToDoList/ToDo.swift
@@ -6,3 +6,12 @@
 //
 
 import Foundation
+struct ToDo: Identifiable {
+    //    var id: ObjectIdentifier // Created by fixing fix on the error. It created a variable but a better practice is a constant
+    let id = UUID().uuidString // Let Swift assing a unique ObjectIndentifier using UUID().uuid String ensures that it is a string
+    var item = ""
+    var remindeerIsOn = false
+    var dueDate = Date.now + (60*60*24)
+    var notes = ""
+    var isCompleted = false
+}

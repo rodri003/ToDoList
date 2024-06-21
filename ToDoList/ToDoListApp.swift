fe5959c3ae437b2c87be854065bbcbb9328a16a7
Rafael Rodriguez <rafael.nc@me.com>
2024-06-21 14:57:23 -0400

    Ch. 5.6 Creation of Observable Object to ToDo List VM

--- ToDoList/ToDoListApp.swift
+++ ToDoList/ToDoListApp.swift
@@ -9,9 +9,11 @@ import SwiftUI
 
 @main
 struct ToDoListApp: App {
+    @StateObject var toDosVM = ToDosViewModel()  // created a var of the class so it needs ()
     var body: some Scene {
         WindowGroup {
             ToDoListView()
+                .environmentObject(toDosVM)  // Makes it available to all the Views that come after this
         }
     }
 }
--- ToDoList/ToDosViewModel.swift
+++ ToDoList/ToDosViewModel.swift
@@ -6,3 +6,7 @@
 //
 
 import Foundation
+
+class ToDosViewModel: ObservableObject {
+    @Published var toDos: [ToDo] = []
+}
